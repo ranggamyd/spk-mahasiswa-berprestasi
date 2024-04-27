00b137f8b7da5e9e3f234f4a6b1468f9e14c8598
Rangga Manggala Yudha <ranggaamyd@gmail.com>
2024-04-27 16:25:37 +0700

    db

---
+++ db_spk.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2024 at 11:25 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_spk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `nama`, `username`, `password`) VALUES
+(1, 'Fadil', 'admin', 'admin'),
+(2, 'angga', 'admin2', 'admin2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_alternatif`
+--
+
+CREATE TABLE `tbl_alternatif` (
+  `id_alternatif` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `kelas` varchar(60) NOT NULL,
+  `nis` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_bobot`
+--
+
+CREATE TABLE `tbl_bobot` (
+  `id_bobot` int(11) NOT NULL,
+  `w1` double NOT NULL,
+  `w2` double NOT NULL,
+  `w3` double NOT NULL,
+  `w4` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_bobot`
+--
+
+INSERT INTO `tbl_bobot` (`id_bobot`, `w1`, `w2`, `w3`, `w4`) VALUES
+(1, 0.35, 0.25, 0.25, 0.15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_nilai`
+--
+
+CREATE TABLE `tbl_nilai` (
+  `id_nilai` int(11) NOT NULL,
+  `id_alternatif` int(11) NOT NULL,
+  `c1` int(11) NOT NULL,
+  `c2` int(11) NOT NULL,
+  `c3` int(11) NOT NULL,
+  `c4` int(11) NOT NULL,
+  `skor` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tbl_alternatif`
+--
+ALTER TABLE `tbl_alternatif`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- Indexes for table `tbl_bobot`
+--
+ALTER TABLE `tbl_bobot`
+  ADD PRIMARY KEY (`id_bobot`);
+
+--
+-- Indexes for table `tbl_nilai`
+--
+ALTER TABLE `tbl_nilai`
+  ADD PRIMARY KEY (`id_nilai`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_alternatif`
+--
+ALTER TABLE `tbl_alternatif`
+  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tbl_bobot`
+--
+ALTER TABLE `tbl_bobot`
+  MODIFY `id_bobot` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_nilai`
+--
+ALTER TABLE `tbl_nilai`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
